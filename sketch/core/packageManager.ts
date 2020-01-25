7d8542b9fde2a9de820358bc1842601162ffbb27
agray5 <grayaprild@gmail.com>
2020-01-25 00:50:24 -0600

    update package manager

--- sketch/core/packageManager.ts
+++ sketch/core/packageManager.ts
@@ -1,23 +1,38 @@
+import uuid from "uuid/v4";
 import Package from "./package";
 import { onEach } from "../utils";
+<<<<<<< Updated upstream
+=======
+import Graph from "../utils/structures/Graph";
+import Module from "./module";
+import { forceArray } from "../utils/list";
+>>>>>>> Stashed changes
 
+export type System = (pm: PackageManager) => void
 export default class PackageManager {
-  packages!: Record<string, Package>;
+  //Package id on top, Component name on bottom
+  map: Graph<Module>
+  systems: Array<System>
 
-  constructor(packages: Record<string, Package> = {}) {
-    this.packages = packages;
+  constructor({systems}: {systems: Array<System>}) {
+    this.map = new Graph();
+    this.systems = systems;
   }
 
-  add (pack: Package) {
-    this.packages[pack.id] = pack;
+  add (mods: Module[]) {
+    const id = uuid();
+    this.map.addElementTop(id, mods);
+    mods.forEach(mod => this.map.addElementBottom(mod.name, mod));
+
     return this;
   }
 
-  remove (pack: Package) {
-    delete this.packages[pack.id];
-    return this;
+  /** Get modules by package id */
+  get (id: string) {
+    return this.map.getTop(id);
   }
 
+<<<<<<< Updated upstream
   update () {
     onEach(this.packages, key => this.packages[key].update())
     return this;
@@ -30,6 +45,32 @@ export default class PackageManager {
 
   mousePressed () {
     onEach(this.packages, key => this.packages[key].mousePressed())
+=======
+  //TODO: ALLOW MODIFICATION OF ALL MODULES IN A PACKAGE (GET SIBLINGS)
+  getByMod (names: string | string[]) {
+    const mods = forceArray(names);
+    const uMods = new Set<Module>();
+
+    mods.forEach(name => {
+      this
+        .map
+        .getBottom(name)
+        .forEach(mod => uMods.add(mod))
+    });
+
+    return Array.from(uMods);
+  }
+
+  remove (id) {
+    //TODO
+    //this.map.remove
+>>>>>>> Stashed changes
     return this;
   }
+
+  update () {
+    this.systems.forEach(system => system(this));
+  }
+
+  
 }
